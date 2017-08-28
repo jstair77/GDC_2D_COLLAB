4528f6b5b026f1c2f2552061111e440c93397ab0
jstair <jstair@iastate.edu>
2017-08-28 17:18:35 -0500

    did some background work for the sky, designed the window hallway

---
+++ GDC 2D Megamix/Assets/STAIR/Scripts/STAIR_FitToCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class STAIR_FitToCamera : MonoBehaviour {
+
+	private Camera cam;
+	public float helper;
+
+	// Use this for initialization
+	void Start () {
+		cam = transform.parent.GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = new Vector3 (cam.pixelHeight / helper, cam.pixelHeight / helper, 1);
+	}
+}
